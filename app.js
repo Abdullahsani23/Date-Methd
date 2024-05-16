b4c392fbb2f65c8958343146815cea25e5516ea7
Abdullahsani23 <abdullahsani95@gmail.com>
2024-05-16 12:17:36 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let a = "Write a program that displays current date and time in your browser."
+document.write(a, "<br>")
+
+var now = new Date()
+document.write(now)
+document.write("<br>" ,"<br>")
+
+let b ="Write a program that alerts the current month in words. For example December."
+document.write(b)
+
+var month = now.getMonth()
+document.write(month)
